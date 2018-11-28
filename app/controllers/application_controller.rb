a20696b43de41c171d4a49a904271e361c63a48f
sandulescumadalina <madalina.sandulescu@hotmail.com>
2018-11-28 20:25:25 +0200

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-  	render html: "Bun venit pe site-ul meu!"
-  end
+  include SessionsHelper
 end
